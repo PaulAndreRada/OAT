a24997563abf589133920b25984032d55e995dd7
PaulAndreRada <paulriverarada@gmail.com>
2014-07-30 20:19:55 -0700

    Cleaned up some comments

--- src/OAT.2.0.js
+++ src/OAT.2.0.js
@@ -7,6 +7,21 @@
  * Name : Object App Template ( OAT )
  * Classification : Structural
  *
+ * Description :
+ * 
+ *  A design pattern intended to streamline the writing process javascript modules [ for front end apps ],
+ *  reducing redundancy by abstracting the most used settings and helper functions. 
+ *  The pattern is designed to be implemented in a readable stream of functions, organized in object literal notation.
+ *  It is currently illustrated using several jQuery Methods but the pattern could easily be made into a stand alone 
+ *  app by replacing the $.extend method by using custom functions
+ *
+ *    - Provides a simplified and easy to read template for large bodies of code, contained by a specific context.
+ *    - Creates private variables accessible to all methods inside the specific context.
+ *    - Sets a common language template for variables inside each object.
+ *    - Stores the common denominator functions for all the apps in one script file, page or module. ( helpers )
+ * 
+ *  inspired by Douglas Crockford’s functional Object pattern
+ *
 */
 
 
@@ -21,12 +36,15 @@
     
 	options = options || {};
     
-	// Add any recurring script settings inside the oat settings or ater them 
-	// when initializing the OAT inside the app
+	// Add any recurring script variables inside the oat settings
+	// to use them globally while app specific variables can be added 
+	// to each scope separetly while maintaining variable privacy
+	// by using the options argument
 	var that = {
 	    settings : {
 		templateLocationId : null,
 		templateTargetId : null,
+		// Optional :
 		// Create an abstraction on top of jQuery's ajax 
 		// method to set your projects most recurring options as default.
 		// using the helper ajax request to set recurring callback methods
@@ -43,11 +61,11 @@
 		}
 	    }
 	};
-    
-    
+    	
+    	
 	// Merge the options ( inherited object ) with the settings object
 	that.settings = $.extend( true, that.settings, options )
-    
+    	
 	// Add all the shared helper methods 
 	that.helper = {
 	
@@ -64,18 +82,17 @@
     
     
 	return that; 
-
+	//
     }; // OAT 
 
 
 
 
     /* Applying the OAT */ 
-
-
-    
     var myApp = function(){ 
-	
+	//
+	// Set the app specific variables and replace global settings only on this scope by
+	// adding an object to the OAT argument
 	var that = OAT({ 
 		templateLocationId : '#headerTemplate',
 		templateTargetId : null,
@@ -84,17 +101,12 @@
 		    newSetting : 'bar'
 		}
 	    }),
-	MA = that,
+	// 
+	// Maintain a dynamic object by using a simple closure
+	MA = ( function(){ return that; }() ),
+	//
+	// save the now dynamic settings into a simple name
 	s = MA.settings;
-
-
-	MA.refreshElements = function(){ 
-	    // 
-	    $doc = $(document),
-	    s.$header = $doc.find( '#' s.headerId );
-	    //
-	};
-	
 	
 	MA.init = function(){ 
 	    //
@@ -107,7 +119,6 @@
 	    return MA;
 	};
 	
-
 	
 	MA.doSomething = function(){ 
 	    //
@@ -125,13 +136,10 @@
 	
        
 	return MA;
-	
+	//
     };//MyApp
-    
-
-
-
-
+    //
+    // Initialise all the apps at the end after all the script dependencies have loaded
     MyApp.init();
     
 
